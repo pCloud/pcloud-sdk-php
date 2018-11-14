03edd7cdea365bbc7b14a310f2a334ee8c14510e
Gabriel Syarov <gabriel.syarov@pcloud.com>
2018-11-14 12:08:42 +0200

    fix

--- lib/pCloud/Config.php
+++ lib/pCloud/Config.php
@@ -3,7 +3,7 @@
 namespace pCloud;
 
 class Config {
-	static $credentialPath = "./app.cred";
+	static $credentialPath = __DIR__ . DIRECTORY_SEPARATOR . "/app.cred";
 	static $host = "https://api.pcloud.com/";
 	static $curllib = "pCloud\Curl";
 	static $filePartSize = 10485760;
